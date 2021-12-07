1081ab02ff0827abc0e2d6c75499b1bfb990beea
rohan-04 <mathurrohan04@gmail.com>
2021-12-07 13:36:26 +0530

    refactor: useState in register component

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Register.css';
 
 const Register = () => {
+  const [id, setId] = useState(0);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventdefault();
+    // send data to the sever
+  };
+
   return (
     <div className='contaniner'>
       <h2>Register</h2>
-      <input placeholder='Name' id='name' />
-      <input placeholder='Email' id='email' />
-      <input placeholder='Passwors' id='password' />
+      <input placeholder='ID' id='id' value={id} />
+      <input placeholder='Name' id='name' value={name} />
+      <input placeholder='Email' id='email' value={email} />
+      <input placeholder='Username' id='username' value={username} />
+      <input placeholder='Password' id='password' value={password} />
+      <button type='subimt' className='btn'>
+        Submit
+      </button>
     </div>
   );
 };
